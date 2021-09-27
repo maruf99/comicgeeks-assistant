a2cd3df67725e3ddcf0e51fa82b53233b9e2757b
maruf99 <25273018+maruf99@users.noreply.github.com>
2021-09-26 21:12:31 -0400

    fix: use raw value instead of enum

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ declare module '@assistant/conversation' {
 
 const assistant = conversation();
 
-assistant.handle(Events.SetUsername, handleUsername);
-assistant.handle(Events.ViewPullList, handlePullList);
+assistant.handle('setUsername', handleUsername);
+assistant.handle('viewPullList', handlePullList);
 
 const handler: VercelApiHandler = (req, res) => {
 	assistant(req, res);
